b8ea50e72b91fb19e92d6649e7c27fd5e8cd9cba
csrgnk <65069854+csrgnk@users.noreply.github.com>
2025-10-08 15:02:48 +0530

    Create 081025.my-first-post.m

---
+++ _posts/081025.my-first-post.m
@@ -0,0 +1,10 @@
+---
+layout: post
+title: My First Post
+date: 2025-10-08
+focus_keyword: my first post
+meta_description: This is the first post description
+meta_keywords: example, first, post
+---
+
+<p>Hello world! This is my first post content.</p>
